10470d19fd391ed81112806afa3f3b33eb2811d6
Leanne <leannestranks86@hotmail.com>
2021-01-20 21:47:39 +1000

    Updated API

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends React.Component {
   componentDidMount () {
     this.setState({ loading: true})
 
-    fetch('https://api.github.com/users')
+    fetch(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
     .then(response => response.json())
     .then(data => this.setState({users: data, loading: false}));
   }
