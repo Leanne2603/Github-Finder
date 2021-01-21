8e2b46aa8a8ad1eaba81049d611a99621e43a335
Leanne <leannestranks86@hotmail.com>
2021-01-21 21:04:56 +1000

    added in search form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './components/layout/Navbar'
 import './App.css';
 import Users from './components/users/Users'
+import Search from './components/users/Search'
 
 
 class App extends React.Component {
@@ -18,11 +19,19 @@ class App extends React.Component {
     .then(data => this.setState({users: data, loading: false}));
   }
 
+  searchUsers = async (text) => {
+    this.setState({ loading: true })
+    fetch(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+    .then(response => response.json())
+    .then(data => this.setState({users: data.items, loading: false}));
+  }
+
   render() {
     return (
       <div className="App">
         <Navbar />
         <div className="container">
+          <Search searchUsers={this.searchUsers} />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
