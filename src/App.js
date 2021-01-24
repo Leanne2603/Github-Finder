aa8c0fce3ac6d03874291581b8c0dffa37799195
Leanne <leannestranks86@hotmail.com>
2021-01-24 20:37:57 +1000

    Updated page layout for user search results

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import Search from './components/users/Search'
 import Alert from './components/layout/Alert'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import About from './components/pages/About'
+import User from './components/users/User'
 
 
 class App extends React.Component {
   state = {
     users: [],
+    user: {},
     loading: false,
     alert: null,
   }
@@ -30,6 +32,14 @@ class App extends React.Component {
     .then(data => this.setState({users: data.items, loading: false}));
   }
 
+  // Get a single Github user
+  getUser = async (login) => {
+    this.setState({ loading: true })
+    fetch(`https://api.github.com/users/${login}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+    .then(response => response.json())
+    .then(data => this.setState({user: data, loading: false}));
+  }
+
   // Clears users from state
   clearUsers = () => this.setState({ users: [], loading: false})
 
@@ -40,7 +50,7 @@ class App extends React.Component {
   }
 
   render() {
-    const {users, loading} = this.state
+    const {users, user, loading} = this.state
 
     return (
       <Router>
@@ -61,6 +71,9 @@ class App extends React.Component {
             </Fragment>
           )} />
           <Route exact path="/about" component={About} />
+          <Route exact path="/user/:login" render ={props => (
+            <User {...props} getUser={this.getUser} user={user} loading={loading} />
+          )} />
         </Switch>
         <div className="container">
         </div>
